8d48d6b0c5fc4170b1053b32ae8b84de45d4ee46
Ori <ori.vaisman1@gmail.com>
2023-03-13 16:45:27 +0200

    modified:   __main__.py

--- __main__.py
+++ __main__.py
@@ -57,6 +57,25 @@ def copy_to_bucket(bucket_from_name, bucket_to_name, file_name):
     }
     s3_resource.Object(bucket_to_name, file_name).copy(copy_source)
 
+def enable_bucket_versioning(bucket_name):
+    """
+    need to add docstring
+    """
+    bkt_versioning = s3_resource.BucketVersioning(bucket_name)
+    bkt_versioning.enable()
+    print(bkt_versioning.status)
+
+def delete_all_objects(bucket_name):
+    """
+    need to add docstring
+    """
+    res = []
+    bucket=s3_resource.Bucket(bucket_name)
+    for obj_version in bucket.object_versions.all():
+        res.append({'Key': obj_version.object_key,
+                    'VersionId': obj_version.id})
+    print(res)
+    bucket.delete_objects(Delete={'Objects': res})
 
                               #                 #
                              ##                 ##
@@ -95,7 +114,67 @@ copy_to_bucket(first_bucket_name, second_bucket_name, first_file_name)
 
 s3_resource.Object(second_bucket_name, first_file_name).delete()
 
+# creating a new file in the first bucket and making it public:
 second_file_name = create_temp_file(400, 'secondfile.txt', 's')
 second_object = s3_resource.Object(first_bucket.name, second_file_name)
 second_object.upload_file(second_file_name, ExtraArgs={
                           'ACL': 'public-read'})
+
+# loading the file's ACL into a variable, then showing it:
+second_object_acl = second_object.Acl()
+print(second_object_acl.grants)
+
+# changing the file's premissions to privte again:
+response = second_object_acl.put(ACL='private')
+print(second_object_acl.grants)
+
+# creating another file in the first bucket and encrypting it:
+third_file_name = create_temp_file(300, 'thirdfile.txt', 't')
+third_object = s3_resource.Object(first_bucket_name, third_file_name)
+third_object.upload_file(third_file_name, ExtraArgs={
+                         'ServerSideEncryption': 'AES256'})
+print(third_object.server_side_encryption)
+
+# re-uploading the third file again but as a different storage class 
+# (changes the way the file is accessed)
+third_object.upload_file(third_file_name, ExtraArgs={
+                         'ServerSideEncryption': 'AES256', 
+                         'StorageClass': 'STANDARD_IA'})
+third_object.reload()
+print(third_object.storage_class)
+
+# enables versioning of files in the first 
+# bucket by calling to a previously configured function
+enable_bucket_versioning(first_bucket_name)
+
+s3_resource.Object(first_bucket_name, second_file_name).upload_file(
+    second_file_name)
+print(s3_resource.Object(first_bucket_name, first_file_name).version_id)
+
+# printing all bucket names using the s3 resource 
+for bucket in s3_resource.buckets.all():
+    print(bucket.name)
+
+# printing all bucket names using the s3 client:
+for bucket_dict in s3_resource.meta.client.list_buckets().get('Buckets'):
+    print(bucket_dict['Name'])
+
+# printing all of the file names in the first bucket
+for obj in first_bucket.objects.all():
+    print(obj.key)
+
+
+for obj in first_bucket.objects.all():
+    subsrc = obj.Object()
+    print(obj.key, obj.storage_class, obj.last_modified,
+          subsrc.version_id, subsrc.metadata)
+
+# calling a previously configured function that deletes all files in the bucket:
+delete_all_objects(first_bucket_name)
+
+s3_resource.Object(second_bucket_name, first_file_name).upload_file(
+    first_file_name)
+delete_all_objects(second_bucket_name)
+
+s3_resource.Bucket(first_bucket_name).delete()
+s3_resource.meta.client.delete_bucket(Bucket=second_bucket_name)
