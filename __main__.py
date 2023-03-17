fac3d96ffff2f1fac286cd8217da0231f73af4a2
vaismanius <ori.vaisman1@gmail.com>
2023-03-17 14:50:51 +0200

    deleted:    2do.txt 	modified:   __main__.py

--- __main__.py
+++ __main__.py
@@ -89,29 +89,43 @@ s3_resource = boto3.resource('s3')
 
 # creating two buckets, the first with a "client" connection type
 # and the second with a "resource" connection type
+try:
+    first_bucket_name, first_response = create_bucket(
+        bucket_prefix='firstpythonbucket', 
+        s3_connection=s3_resource.meta.client)
+except Exception as e:
+    print("An exception has occurred:", type(e).__name__)
 
-first_bucket_name, first_response = create_bucket(
-    bucket_prefix='firstpythonbucket', 
-    s3_connection=s3_resource.meta.client)
-
-second_bucket_name, second_response = create_bucket(      
-    bucket_prefix='secondpythonbucket', s3_connection=s3_resource)
+try:
+    second_bucket_name, second_response = create_bucket(      
+        bucket_prefix='secondpythonbucket', s3_connection=s3_resource)
+except Exception as e:
+    print("An exception has occurred:", type(e).__name__)
 
 
 # creating a file by calling to the "create_temp_file" function:
 first_file_name = create_temp_file(300, 'firstfile.txt', 'f')   
 
+# uploading the first file to the first bucket
 first_bucket = s3_resource.Bucket(name=first_bucket_name)
 first_object = s3_resource.Object(
     bucket_name=first_bucket_name, key=first_file_name)
+try:
+    first_object.upload_file(first_file_name)
+except Exception as e:
+    print("An exception has occurred:", type(e).__name__)
 
-first_object.upload_file(first_file_name)
+# downloading the first file that we just uploaded
+try:
+    s3_resource.Object(first_bucket_name, first_file_name).download_file(
+        f'/tmp/{first_file_name}')
+except Exception as e:
+    print("An exception has occurred:", type(e).__name__)
 
-s3_resource.Object(first_bucket_name, first_file_name).download_file(
-    f'/tmp/{first_file_name}')
-
+# copying the first file from the first bucket to the second bucket
 copy_to_bucket(first_bucket_name, second_bucket_name, first_file_name)
 
+# deleting the file we just copied to the second bucket
 s3_resource.Object(second_bucket_name, first_file_name).delete()
 
 # creating a new file in the first bucket and making it public:
@@ -159,10 +173,9 @@ for bucket in s3_resource.buckets.all():
 for bucket_dict in s3_resource.meta.client.list_buckets().get('Buckets'):
     print(bucket_dict['Name'])
 
-# printing all of the file names in the first bucket
+# two methods for printing all of the file names in the first bucket
 for obj in first_bucket.objects.all():
     print(obj.key)
-
 
 for obj in first_bucket.objects.all():
     subsrc = obj.Object()
@@ -170,11 +183,23 @@ for obj in first_bucket.objects.all():
           subsrc.version_id, subsrc.metadata)
 
 # calling a previously configured function that deletes all files in the bucket:
-delete_all_objects(first_bucket_name)
+try:
+    delete_all_objects(first_bucket_name)
+except Exception as e:
+    print("An exception has occurred:", type(e).__name__)
 
+# uploading an alreadt existing file to the second bucket to see what happens in
+# a bucket that is not version enabled (it's version will be null)
 s3_resource.Object(second_bucket_name, first_file_name).upload_file(
     first_file_name)
-delete_all_objects(second_bucket_name)
+try:
+    delete_all_objects(second_bucket_name)
+except Exception as e:
+    print("An exception has occurred:", type(e).__name__)
 
-s3_resource.Bucket(first_bucket_name).delete()
-s3_resource.meta.client.delete_bucket(Bucket=second_bucket_name)
+# deleting the fisrt & second bucket
+try:
+    s3_resource.Bucket(first_bucket_name).delete()
+    s3_resource.meta.client.delete_bucket(Bucket=second_bucket_name)
+except Exception as e:
+    print("An exception has occurred:", type(e).__name__)
